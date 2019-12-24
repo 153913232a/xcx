ac41fe27f3e037ac242522b125e9e3f0a83dfbab
unknown <unknown@example.com>
2019-12-25 00:05:24 +0800

    基本完成
    基本完成

--- pages/my/my-car/my-car.js
+++ pages/my/my-car/my-car.js
@@ -1,48 +1,73 @@
 // pages/my/my-car/my-car.js
+const app = getApp()
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-   cars: [
-      {
-        id: '1',
-        name: 'A',
-        price: '30',
-        url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
-      },
-      {
-        id: '2',
-        name: 'B',
-        price: '35',
-        url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
-      },
-      {
-        id: '3',
-        name: 'C',
-        price: '40',
-        url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
-      }, {
-        id: '4',
-        name: 'D',
-        price: '45',
-        url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
-      }
+    firstPhoto: [],
+    cars: [
+      // {
+      //   id: '1',
+      //   name: 'A',
+      //   price: '30',
+      //   url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
+      // },
+      // {
+      //   id: '2',
+      //   name: 'B',
+      //   price: '35',
+      //   url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
+      // },
+      // {
+      //   id: '3',
+      //   name: 'C',
+      //   price: '40',
+      //   url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
+      // }, {
+      //   id: '4',
+      //   name: 'D',
+      //   price: '45',
+      //   url: 'https://ossweb-img.qq.com/images/lol/img/champion/Morgana.png'
+      // }
     ],
     content: "我的车辆详情"
   },
 
   toDetail(e) {
+    console.log(e)
+    let car = e.currentTarget.dataset.target
     wx.navigateTo({
-      url: "/pages/component/home-detai/home-detail?type=1",
+      url: "/pages/component/home-detai/home-detail?type=1&car=" + JSON.stringify(car),
     })
   },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    wx.request({
+      url: 'http://localhost:9093/car/getCars',
+      data: {
+        openId: app.globalData.userInfo.openId
+      },
+      success: function (res) {
+        console.log(res)
+        // let list = res.data.data.carTypes
+        that.setData({
+          cars: res.data.data
+        })
+        let fist=[]
+        that.data.cars.forEach(function(item) {
+          fist.push(item.photo_addr.split(',')[0])
+        })
+        console.log(fist)
+        that.setData({
+          firstPhoto: fist
+        })
+      }
+    })
   },
 
   /**
